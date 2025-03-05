94d14e1ac17a3f052a87202b715721dec7904998
Ken-Anazodo <kenanazodo43@gmail.com>
2025-03-05 10:49:35 +0100

    ifolder removed

--- instance/config.py
+++
@@ -1,4 +0,0 @@
-SECRET_KEY='randomsecretkey'
-# SQLALCHEMY_DATABASE_URI='dbdialet+dbdriver://username:password@127.0.0.1:3306/dbname'
-SQLALCHEMY_DATABASE_URI='mysql+mysqlconnector://root@127.0.0.1:3306/sightsee'
-SQLALCHEMY_TRACK_MODIFICATIONS=False
